86cfd29912ba8ce44b46b136e45f66cf69417f17
Risto Keravuori <rkeravuori@fool.com>
2014-05-19 15:58:02 -0400

    more config

--- config.js
+++ config.js
@@ -11,20 +11,20 @@ config = {
         // The url to use when providing links to the site, E.g. in RSS and email.
         url: 'http://my-ghost-blog.com',
 
-        // Example mail config
-        // Visit http://docs.ghost.org/mail for instructions
-        // ```
-        //  mail: {
-        //      transport: 'SMTP',
-        //      options: {
-        //          service: 'Mailgun',
-        //          auth: {
-        //              user: '', // mailgun username
-        //              pass: ''  // mailgun password
-        //          }
-        //      }
-        //  },
-        // ```
+//         Example mail config
+//         Visit http://docs.ghost.org/mail for instructions
+//         ```
+//          mail: {
+//              transport: 'SMTP',
+//              options: {
+//                  service: 'Mailgun',
+//                  auth: {
+//                      user: '', // mailgun username
+//                      pass: ''  // mailgun password
+//                  }
+//              }
+//          },
+//         ```
 
         database: {
             client: 'sqlite3',
@@ -49,9 +49,18 @@ config = {
     // Configure your URL and mail settings here
     production: {
         url: 'http://ghosto.herokuapp.com',
-        mail: {},
+        mail: {
+            transport: 'SMTP',
+              options: {
+                  service: 'risto.io',
+                  auth: {
+                      user: process.env.MAIL_USER,
+                      pass: process.env.MAIL_PASSWORD
+                  }
+              }
+        },
         database: {
-            client: 'postgres',
+            client: 'pg',
             connection: {
                 host: process.env.POSTGRES_HOST,
                 user: process.env.POSTGRES_USER,
@@ -68,85 +77,6 @@ config = {
             port: process.env.PORT
         },
         fileStorage: false
-    },
-
-    // **Developers only need to edit below here**
-
-    // ### Testing
-    // Used when developing Ghost to run tests and check the health of Ghost
-    // Uses a different port number
-    testing: {
-        url: 'http://127.0.0.1:2369',
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost-test.db')
-            }
-        },
-        server: {
-            host: '127.0.0.1',
-            port: '2369'
-        },
-        logging: false
-    },
-
-    // ### Travis
-    // Automated testing run through GitHub
-    'travis-sqlite3': {
-        url: 'http://127.0.0.1:2369',
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost-travis.db')
-            }
-        },
-        server: {
-            host: '127.0.0.1',
-            port: '2369'
-        },
-        logging: false
-    },
-
-    // ### Travis
-    // Automated testing run through GitHub
-    'travis-mysql': {
-        url: 'http://127.0.0.1:2369',
-        database: {
-            client: 'mysql',
-            connection: {
-                host     : '127.0.0.1',
-                user     : 'travis',
-                password : '',
-                database : 'ghost_travis',
-                charset  : 'utf8'
-            }
-        },
-        server: {
-            host: '127.0.0.1',
-            port: '2369'
-        },
-        logging: false
-    },
-
-    // ### Travis
-    // Automated testing run through GitHub
-    'travis-pg': {
-        url: 'http://127.0.0.1:2369',
-        database: {
-            client: 'pg',
-            connection: {
-                host     : '127.0.0.1',
-                user     : 'postgres',
-                password : '',
-                database : 'ghost_travis',
-                charset  : 'utf8'
-            }
-        },
-        server: {
-            host: '127.0.0.1',
-            port: '2369'
-        },
-        logging: false
     }
 };
 
